669ff73dcd22cee367c3401f5130282ebf61aff5
MusfiqDehan <musfiqdehan@gmail.com>
2026-06-20 09:55:13 +0600

    Add URL routes for tenant feature overrides and user listing
    - Introduced TenantFeatureOverrideView for managing tenant-specific feature overrides.
    - Added TenantUserListView for retrieving a list of users associated with a tenant.

--- apps/tenancy/urls/tenant.py
+++ apps/tenancy/urls/tenant.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
 from apps.tenancy.views import ChangePasswordView, CurrentTenantFeaturesView, MeView
+from apps.tenancy.views.users import TenantUserListView
 
 app_name = "tenancy"
 
@@ -12,4 +13,5 @@ urlpatterns = [
         name="current-tenant-features",
     ),
     path("password/change/", ChangePasswordView.as_view(), name="password-change"),
+    path("users/", TenantUserListView.as_view(), name="tenant-user-list"),
 ]
