b184756c0da1813d73ff40b9746e61f4ce8f9758
MusfiqDehan <musfiqdehan@gmail.com>
2026-06-25 14:47:53 +0600

    Add `ready` method to SharedConfig for signal imports
    - Implemented a `ready` method in the `SharedConfig` class to import `shared.cache.signals`, ensuring that signal handlers are registered when the application starts.
    - This addition enhances the initialization process of the shared application configuration, improving the management of cache signals.

--- shared/apps.py
+++ shared/apps.py
@@ -5,3 +5,6 @@ class SharedConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "shared"
     label = "shared"
+
+    def ready(self) -> None:
+        import shared.cache.signals  # noqa: F401
