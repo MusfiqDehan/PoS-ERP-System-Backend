e763ee50935ae1ecbb9bc23ce0f0744d23288bf3
MusfiqDehan <musfiqdehan@gmail.com>
2026-06-25 14:24:08 +0600

    Add services module for platform_owner app
    - Introduced a new `__init__.py` file in the services directory to define the public interface for various services used in the platform_owner app. This change organizes the services for authentication, features, invitations, and user management, enhancing code structure and maintainability.

---
+++ apps/platform_owner/services/__init__.py
@@ -0,0 +1,11 @@
+from apps.platform_owner.services.auth import PlatformAuthService
+from apps.platform_owner.services.features import PlatformFeatureService
+from apps.platform_owner.services.invitation import PlatformInvitationService
+from apps.platform_owner.services.users import PlatformUserService
+
+__all__ = [
+    "PlatformAuthService",
+    "PlatformFeatureService",
+    "PlatformInvitationService",
+    "PlatformUserService",
+]
